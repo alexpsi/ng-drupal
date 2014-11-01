2d7d7f4f76c963373d4524cd59a83ad1d97d1339
alexpsi <alexpsi256@gmail.com>
2014-11-01 18:47:04 +0200

    Refactored service as a provider so it can be configured

--- ngDrupal.js
+++ ngDrupal.js
@@ -1,121 +1,134 @@
 'use strict';
 
 /**
- * @ngdoc service
- * @name ngDrupal
+ * @ngdoc function
+ * @name recApp.controller:UserCtrl
  * @description
- * # ngDrupal 
- * Angular service that allows Restangular to connect to a Drupal site utilizing Services 3.5+. 
- * Provides authentication and file upload methods.
+ * # UserCtrl
+ * Controller of the recApp
  */
+
 angular.module('ngDrupal', []) 
-.factory('ngDrupal', function($q, $http, Restangular) {
-    
-    var ngDrupal = {};
+    .provider('ngDrupal', function() {
+        // In the provider function, you cannot inject any
+        // service or factory. This can only be done at the
+        // "$get" method.
+     
+        this.hostname = '';
+        this.endpoint = '';
 
-    ngDrupal.setRestPath = function (root, endpoint) {
-        
-        Restangular.setBaseUrl('HOSTNAME'); //Need to add service configuration
-        ngDrupal.SITE_ROOT = root;
-        ngDrupal.SERVICES_ENDPOINT = endpoint;
-        ngDrupal.REST_PATH = root + endpoint + '/';
-        Restangular.setBaseUrl(ngDrupal.REST_PATH);
-        Restangular.setDefaultHttpFields({withCredentials: true});
+        this.$get = function($q, $http, Restangular) {
 
-    };
+            var SITE_ROOT = this.hostname;
+            var SERVICES_ENDPOINT = this.endpoint;
+            var REST_PATH = SITE_ROOT + SERVICES_ENDPOINT + '/';
+            Restangular.setBaseUrl(REST_PATH);
+            Restangular.setDefaultHttpFields({withCredentials: true});
+            //console.log(REST_PATH);
+            return {
 
-    ngDrupal.getCsrfToken = function() {
-        
-        var deferred = $q.defer();
-        $http({
-            url: ngDrupal.SITE_ROOT + 'services/session/token',
-            method: 'GET',
-            withCredentials: true
-        }).success(function(data /*, status, headers, config*/) {
-            $http.defaults.headers.common['X-CSRF-Token'] = data;
-            deferred.resolve(data);
-        }).error(function(data /*, status, headers, config*/) {
-            deferred.reject(data);
-        });
-        
-        return deferred.promise;
-    };
+                getCsrfToken: function() {
+                    
+                    var deferred = $q.defer();
+                    $http({
+                        url: SITE_ROOT + 'services/session/token',
+                        method: 'GET',
+                        withCredentials: true
+                    }).success(function(data /*, status, headers, config*/) {
+                        $http.defaults.headers.common['X-CSRF-Token'] = data;
+                        deferred.resolve(data);
+                    }).error(function(data /*, status, headers, config*/) {
+                        deferred.reject(data);
+                    });
+                    return deferred.promise;
+                },
 
-    ngDrupal.systemConnect = function() {
-        var deferred = $q.defer();
-        var url = ngDrupal.REST_PATH + 'system/connect';
-        ngDrupal.getCsrfToken().then(function() {
-            $http({
-                url: url,
-                method: 'POST',
-                withCredentials: true
-            })
-            .success(function(data /*, status, headers, config */) { 
-                deferred.resolve(data.user);
-            })
-            .error(function(data /*, status, headers, config */) {
-                deferred.reject(data);
-            });        
-        });
-        return deferred.promise;
-    };
+                systemConnect: function() {
+                    
+                    var deferred = $q.defer();
+                    var url = REST_PATH + 'system/connect';
+                    this.getCsrfToken().then(function() {
+                        $http({
+                            url: url,
+                            method: 'POST',
+                            withCredentials: true
+                        })
+                        .success(function(data /*, status, headers, config */) { 
+                            deferred.resolve(data.user);
+                        })
+                        .error(function(data /*, status, headers, config */) {
+                            deferred.reject(data);
+                        });        
+                    });
+                    return deferred.promise;
+                },
+
+                logout: function() {
+                    
+                    var that = this;
+                    var deferred = $q.defer();
+                    var url = REST_PATH + 'user/logout';
+                    
+                    $http({
+                        url: url,
+                        method: 'POST',
+                        withCredentials: true
+                    })
+                    .success(function(data /*, status, headers, config */) { 
+                        that.getCsrfToken().then(function() {
+                            deferred.resolve(data);
+                        });
+                        
+                    })
+                    .error(function(data /*, status, headers, config */) {
+                        deferred.reject(data);
+                    });
+                    
+                    return deferred.promise;
+                },
+
+                login: function (username, password) {
+                    
+                    var that = this;
+                    var deferred = $q.defer();
+                    var url = REST_PATH + 'user/login';
+                    var user = {
+                      username: username,
+                      password: password
+                    };
+
+                    $http({
+                      url: url,
+                        method: 'POST',
+                        data: user,
+                        withCredentials: true,
+                    })
+                    .success(function(data /*, status, headers, config */) { 
+                        that.getCsrfToken().then(function() {
+                            deferred.resolve(data);
+                        });
+                        
+                    })
+                    .error(function(data /*, status, headers, config */) {
+                        deferred.reject(data);
+                    });
+                    
+                    return deferred.promise;
+                },
+
+                nodeFactory: function() {
+
+                  return Restangular.all('node');
+                }
 
 
-    ngDrupal.logout = function() {
-        var deferred = $q.defer();
-        var url = ngDrupal.REST_PATH + 'user/logout';
-        
-        $http({
-            url: url,
-            method: 'POST',
-            withCredentials: true
-        })
-        .success(function(data /*, status, headers, config */) { 
-            ngDrupal.getCsrfToken().then(function() {
-                $http.defaults.headers.common['X-CSRF-Token'] = '';
-                deferred.resolve(data);
-            });
-            
-        })
-        .error(function(data /*, status, headers, config */) {
-            deferred.reject(data);
-        });
-        
-        return deferred.promise;
-    };
 
-    ngDrupal.login = function (username, password) {
-        var deferred = $q.defer();
-        var url = ngDrupal.REST_PATH + 'user/login';
-        var user = {
-          username: username,
-          password: password
+
+            }
         };
-
-        $http({
-          url: url,
-            method: 'POST',
-            data: user,
-            withCredentials: true,
-        })
-        .success(function(data /*, status, headers, config */) { 
-            ngDrupal.getCsrfToken().then(function() {
-                deferred.resolve(data);
-            });
-            
-        })
-        .error(function(data /*, status, headers, config */) {
-            deferred.reject(data);
-        });
-        
-        return deferred.promise;
-    };
-
-    ngDrupal.nodeFactory = function() {
-      return Restangular.all('node');
-    };
-
-    return ngDrupal;
+     
+        this.config = function(options) {
+            this.hostname = options.hostname;
+            this.endpoint = options.endpoint;
+        };
 });
-
-
